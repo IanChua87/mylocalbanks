076723a881ef976477e00381e05f6ca5fd363415
22024905@myrp.edu.sg <22024905@myrp.edu.sg>
2023-05-25 18:47:09 +0800

    Commit after challenge

--- app/src/main/java/sg/edu/rp/c346/id22024905/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22024905/mylocalbanks/MainActivity.java
@@ -1,15 +1,21 @@
 package sg.edu.rp.c346.id22024905.mylocalbanks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.LocaleListCompat;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -100,5 +106,33 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_option, menu);
         return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            setLocale("en");
+            return true;
+        } else if (id == R.id.ChineseSelection) {
+            setLocale("zh");
+            return true;
+        } else{
+            setLocale("th");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setLocale(String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        tvDBS.setText(R.string.d_bs);
+        tvOCBC.setText(R.string.o_cbc);
+        tvUOB.setText(R.string.u_ob);
     }
 }
